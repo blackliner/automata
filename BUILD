bdc99b408f5626b4651ddb479689a68574e85c99
Florian Berchtold <florian.berchtold@gmail.com>
2019-12-31 11:33:28 +0100

    reallocated files

--- BUILD
+++ BUILD
@@ -6,86 +6,13 @@ cc_binary(
     name = "auto",
     srcs = [
         "Main.cpp",
-        "lib/Main.h",
+        "Main.h",
     ],
     copts = BUILD_COPTS + ["-Ilib"] + ["-Iquadtree"],
     deps = [
-        "vehicle",
+        "//lib:vehicle",
         "//quadtree:quad_tree",
-        "olc_pixel_game_engine",
+        "//lib:olc_pixel_game_engine",
         #"//lib:vector2d",
     ],
-)
-
-cc_library(
-    name = "renderer",
-    srcs = ["lib/Renderer.h"],
-    copts = BUILD_COPTS,
-    deps = ["vector2d"],
-)
-
-cc_library(
-    name = "vehicle",
-    srcs = ["Vehicle.cpp"],
-    hdrs = ["lib/Vehicle.h"],
-    copts = BUILD_COPTS + ["-Ilib"],
-    deps = [
-        "path_segment",
-        "perlin",
-        "renderer",
-        "weapons",
-    ],
-)
-
-cc_library(
-    name = "olc_pixel_game_engine",
-    hdrs = ["lib/olcPixelGameEngine.h"],
-    copts = BUILD_COPTS,
-    deps = [
-        #"@system_libs//:all", # would work if there was no libpthread_db.so :-(
-        "@system_libs//:pthread",
-        "@system_libs//:x11",
-        "@system_libs//:png",
-        "@system_libs//:gl",
-    ],
-)
-
-cc_library(
-    name = "vector2d",
-    hdrs = ["lib/Vector2D.h"],
-    copts = BUILD_COPTS,  # + ["-Ilib"],
-)
-
-cc_library(
-    name = "weapons",
-    srcs = ["Weapons.cpp"],
-    hdrs = ["lib/Weapons.h"],
-    copts = BUILD_COPTS + ["-Ilib"],
-    deps = ["vector2d"],
-)
-
-cc_library(
-    name = "path_segment",
-    hdrs = ["lib/PathSegment.h"],
-    copts = BUILD_COPTS + ["-Ilib"],
-    deps = ["vector2d"],
-)
-
-cc_library(
-    name = "perlin",
-    hdrs = ["lib/Perlin.h"],
-    copts = BUILD_COPTS,
-)
-
-cc_library(
-    name = "hashtable",
-    hdrs = ["lib/hashtable.h"],
-    copts = BUILD_COPTS,
-    deps = ["hash"],
-)
-
-cc_library(
-    name = "hash",
-    hdrs = ["lib/hash.h"],
-    copts = BUILD_COPTS,
 )
---
+++ lib/matrix2d.h
@@ -0,0 +1,111 @@
+
+
+template <typename T>
+class Matrix_2d {
+  // x = columns = n_input, y = rows = n_output
+ public:
+  std::vector<T>& operator()() noexcept {
+    return m_data;
+  }
+
+  const std::vector<const T>& operator()() noexcept const {
+    return m_data;
+  }
+
+  std::vector<T>& GetData() noexcept {
+    return m_data;
+  }
+
+  const std::vector<const T>& GetData() noexcept const {
+    return m_data;
+  }
+
+  T& operator()(size_t x, size_t y) noexcept {
+    return m_data[x + y * m_columns];
+  }
+
+  const T& operator()(size_t x, size_t y) const noexcept {
+    return m_data[x + y * m_columns];
+  }
+
+  T& operator()(size_t idx) noexcept {
+    return m_data[idx];
+  }
+
+  const T& operator()(size_t idx) const noexcept {
+    return m_data[idx];
+  }
+
+  Matrix_2d& operator=(Matrix_2d other) noexcept {
+    m_data = std::move(other.m_data);
+    m_columns = other.m_columns;
+    m_rows = other.m_rows;
+
+    return *this;
+  }
+
+  Matrix_2d& operator+=(const Matrix_2d& rhs) {
+    assert(m_columns == rhs.m_columns);
+    assert(m_rows == rhs.m_rows);
+
+    for (size_t i{}; i < m_data.size(); ++i) {
+      m_data[i] += rhs.m_data[i];
+    }
+    return *this;
+  }
+
+  Matrix_2d operator+(const Matrix_2d& rhs) {
+    assert(m_columns == rhs.m_columns);
+    assert(m_rows == rhs.m_rows);
+
+    Matrix_2d result;
+    result.Resize(m_columns, m_rows);
+
+    for (size_t i{}; i < m_data.size(); ++i) {
+      result.m_data[i] = m_data[i] + rhs.m_data[i];
+    }
+    return result;
+  }
+
+  void Resize(size_t n_columns, size_t n_rows) {
+    assert(n_rows * n_columns != 0);
+    m_rows = n_rows;
+    m_columns = n_columns;
+    m_data.resize(n_rows * n_columns);
+  }
+
+  void Reset(T new_value = T{}) {
+    for (double& value : m_data) {
+      value = new_value;
+    }
+  }
+
+  constexpr size_t Ninput() const noexcept {
+    return m_columns;
+  }
+
+  constexpr size_t Noutput() const noexcept {
+    return m_rows;
+  }
+
+  constexpr size_t Size() const noexcept {
+    return m_columns * m_rows;
+  }
+
+  void SetWeights(std::initializer_list<T>&& new_data) {
+    m_data = std::move(new_data);
+  }
+
+  void Randomize() {
+    for (auto& value : m_data) {
+      value = static_cast<T>(rand()) / RAND_MAX;
+      value -= 0.5;
+      value *= 2.0;
+    }
+  }
+
+ private:
+  size_t m_columns{};
+  size_t m_rows{};
+  std::vector<T> m_data;
+};
