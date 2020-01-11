f7d4d495bfbc381be6c652e2ab3bbde4c15dce0f
Florian Berchtold <florian.berchtold@gmail.com>
2020-01-12 00:32:50 +0100

    pipeline works now

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -5,11 +5,14 @@ docker run \
     --network=host \
 	--rm \
     --user $UID:$GID \
-    --workdir="/home/$USER" \
+    --workdir="/automata" \
     --volume="/etc/group:/etc/group:ro" \
     --volume="/etc/passwd:/etc/passwd:ro" \
     --volume="/etc/shadow:/etc/shadow:ro" \
     --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+    --volume="/var/run/docker.sock:/var/run/docker.sock:rw" \
+    --volume="/tmp:/tmp:rw" \
     --volume="/home/$USER:/home/$USER:rw" \
+    --volume="${DIR}/..:/automata:rw" \
     "$@" \
 	bazel-build
--- docker/entry.sh
+++ docker/entry.sh
@@ -1,6 +1,8 @@
-cd
-cd workspace
-cd automata
+cd /automata
 
+#bazel clean --expunge
+#bazel sync
+
+bazel clean # this here only works because the directory we are in is the EXACT SAME NAME in the docker and native, otherwise bugs!!!
 bazel build ...
 bazel test ...
