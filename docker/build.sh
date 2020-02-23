3c3bdac022f6e6cc5d49278fc2b9e12df55a0f05
Florian Berchtold <florian.berchtold@gmail.com>
2020-02-23 10:51:23 +0100

    realpath

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,5 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$( dirname $(realpath $0) )"
 
 docker build \
 	--network=host \
