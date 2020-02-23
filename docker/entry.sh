22cc0781baa4b8e435a8ce1ba52cb91826401f1a
Florian Berchtold <florian.berchtold@gmail.com>
2020-02-23 14:38:46 +0100

    some work

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,7 +1,7 @@
 cd /automata
 
-#bazel clean --expunge
-#bazel sync
+# bazel clean --expunge
+# bazel sync
 
 bazel clean # this here only works because the directory we are in is the EXACT SAME NAME in the docker and native, otherwise bugs!!!
 bazel build ...
