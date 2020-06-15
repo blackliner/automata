99aeb4ee4e3d3adff8ecdabdd3813d2fc085d070
Florian Berchtold <florian.berchtold@gmail.com>
2020-06-15 19:15:57 +0200

    install deps

---
+++ install_deps.sh
@@ -0,0 +1,10 @@
+sudo apt update && sudo apt install --no-install-recommends -y \
+libflac-dev \
+libopenal-dev \
+libudev-dev \
+libvorbis-dev \
+libgl1-mesa-dev \
+libx11-dev \
+cmake \
+ninja-build \
+build-essential
